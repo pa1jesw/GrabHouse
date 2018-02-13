f25dadc4b9f2d338d3e7dc883e00946d2c2136af
Muku17 <mukeshwadhwa143@gmail.com>
2018-02-14 00:43:24 +0530

    done with the workflow

--- app/src/main/java/com/fcproject/grabhouce/MainActivity.java
+++ app/src/main/java/com/fcproject/grabhouce/MainActivity.java
@@ -1,13 +1,39 @@
 package com.fcproject.grabhouce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnCreate,btnLogin,btnGoogle;
+    EditText etEmail,etPassword;
+    TextView tvForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnGoogle = findViewById(R.id.btnGoogle);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        tvForgot = findViewById(R.id.tvForgot);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1 = new Intent(MainActivity.this,create_account.class);
+                startActivity(i1);
+            }
+        });
+
+
     }
 }
