6457bedaa4d8e5cd68d709c847845eae7ccee0f5
ADMIN <sewaniharshini@gmail.com>
2018-03-04 02:35:42 +0530

    Connecting to firebase (with new email id)

--- app/src/main/java/com/fcproject/grabhouce/create_account.java
+++ app/src/main/java/com/fcproject/grabhouce/create_account.java
@@ -139,7 +139,7 @@ public class create_account extends AppCompatActivity {
 
     }
     private void signIn()
-            
+
     {
 
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
