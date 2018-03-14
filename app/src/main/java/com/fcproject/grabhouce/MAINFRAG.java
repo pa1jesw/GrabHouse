66fa996a902ef89b88eda4331527d3decad587f8
ADMIN <sewaniharshini@gmail.com>
2018-03-14 22:51:49 +0530

    Recycler view file missing (update)

---
+++ app/src/main/java/com/fcproject/grabhouce/MAINFRAG.java
@@ -0,0 +1,12 @@
+package com.fcproject.grabhouce;
+
+
+import android.support.v4.app.Fragment;
+
+public class MAINFRAG extends purchaseFragment
+{
+   // protected Fragment createFragment()
+    //{
+     //   return new purchaseFragment().newInstance();
+    //}
+}
