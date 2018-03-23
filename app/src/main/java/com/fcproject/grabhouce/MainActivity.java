635252268c455a377c2bbfe9ca2003935ebccbd0
Ravi <immaturegroup@gmail.com>
2018-03-23 15:07:27 +0530

    Ui Changes| CardView Layout is built! [Need some changes in codng section for Tab Layout ]

--- app/src/main/java/com/fcproject/grabhouce/MainActivity.java
+++ app/src/main/java/com/fcproject/grabhouce/MainActivity.java
@@ -6,98 +6,235 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dd.processbutton.iml.ActionProcessButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static java.sql.Types.NULL;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//
+//    Button btnCreate,btnLogin,btnGoogle;
+//    EditText etEmail,etPassword;
+//    private ProgressDialog progressDialog;
+//    private FirebaseAuth mAuth;
+//    TextView tvForgot;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.new_activity_main);
+////
+////        btnLogin = findViewById(R.id.btnLogin);
+////        btnCreate = findViewById(R.id.btnCreate);
+////        btnGoogle = findViewById(R.id.btnGoogle);
+////        etEmail = findViewById(R.id.etEmail);
+////        etPassword = findViewById(R.id.etPassword);
+////        tvForgot = findViewById(R.id.tvForgot);
+////        progressDialog = new ProgressDialog(this);
+////        mAuth = FirebaseAuth.getInstance();
+////
+////        //checking if user is already logged in
+////
+////        if(mAuth.getCurrentUser() != null)
+////        {
+////            //directly go to home if user is already logged in
+////            finish();
+////            startActivity(new Intent(getApplicationContext(),home.class));
+////        }
+////
+////        btnCreate.setOnClickListener(new View.OnClickListener() {
+////            @Override
+////            public void onClick(View view) {
+////                Intent i1 = new Intent(MainActivity.this,create_account.class);
+////                startActivity(i1);
+////            }
+////        });
+////        btnLogin.setOnClickListener(this);
+////
+////
+////    }
+////
+////    private void userLogin(){
+////        String email = etEmail.getText().toString().trim();
+////        String password = etPassword.getText().toString().trim();
+////        if(TextUtils.isEmpty(email)){
+////            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+////            return;
+////        }
+////
+////        if(TextUtils.isEmpty(password)){
+////            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+////            return;
+////        }
+////
+////        //if the email and password are not empty
+////        //displaying a progress dialog
+////
+////        progressDialog.setMessage("Checking credentials Please Wait");
+////        progressDialog.show();
+////
+////        mAuth.signInWithEmailAndPassword(email,password)
+////                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+////                    @Override
+////                    public void onComplete(@NonNull Task<AuthResult> task) {
+////                        if(task.isSuccessful())
+////                        {
+////                            finish();
+////                            startActivity(new Intent(getApplicationContext(),home.class));
+////                        }
+////                    }
+////                });
+////    }
+////
+////    @Override
+////    public void onClick(View view) {
+////        if(view == btnLogin){
+////            userLogin();
+////        }
+//    }
+//}
+public class MainActivity extends AppCompatActivity {
 
-    Button btnCreate,btnLogin,btnGoogle;
-    EditText etEmail,etPassword;
-    private ProgressDialog progressDialog;
+
+    ActionProcessButton btnFirebaseSignUp,btnFirebaseLogin;
+    EditText et_email,et_pass;
     private FirebaseAuth mAuth;
-    TextView tvForgot;
+    FirebaseUser currentuser;
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.new_activity_main);
 
-        btnLogin = findViewById(R.id.btnLogin);
-        btnCreate = findViewById(R.id.btnCreate);
-        btnGoogle = findViewById(R.id.btnGoogle);
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
-        tvForgot = findViewById(R.id.tvForgot);
-        progressDialog = new ProgressDialog(this);
+        btnFirebaseSignUp = findViewById(R.id.btnFirebaseSignUp);
+        btnFirebaseLogin=findViewById(R.id.btnFirebaseLogin);
+        et_email=findViewById(R.id.ed_email);
+        et_pass=findViewById(R.id.ed_password);
         mAuth = FirebaseAuth.getInstance();
-
-        //checking if user is already logged in
-
-        if(mAuth.getCurrentUser() != null)
+            btnFirebaseSignUp.setMode(ActionProcessButton.Mode.ENDLESS);
+    //start with progress = 0
+            btnFirebaseSignUp.setProgress(0);
+        currentuser=FirebaseAuth.getInstance().getCurrentUser();
+        if(currentuser!=null)
         {
-            //directly go to home if user is already logged in
-            finish();
-            startActivity(new Intent(getApplicationContext(),home.class));
+            Intent userintent=new Intent(getApplicationContext(),home.class);
+            userintent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(userintent);
         }
+        else{
+            // Toast.makeText(this, "User is signed out!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i1 = new Intent(MainActivity.this,create_account.class);
-                startActivity(i1);
+    public void firebaseSignUp(View view) {
+
+//        email =et_email.getText().toString();
+//        password=et_pass.getText().toString();
+//
+//        if(password.length()<8)
+//        {
+//            et_pass.setError("Please enter password of minimum length 8");
+//            et_pass.requestFocus();
+//            return;
+//        }
+//        else if(TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+//        {
+//            et_email.setError("Enter Valid Email id");
+//            et_email.requestFocus();
+//            return;
+//        }
+//        else
+//        {
+//            ActionProcessButton btn = (ActionProcessButton) view;
+//            // we add 25 in the button progress each click
+//            if(btn.getProgress() < 100){
+//                btn.setProgress(btn.getProgress() + 15);
+//                et_email.setEnabled(false);
+//                et_pass.setEnabled(false);
+//            }
+//            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+////                            if(currentuser!=null)
+////                            {
+////                                Toast.makeText(LoginActivity.this, "Account created successfully!  with email Id"+email, Toast.LENGTH_SHORT).show();
+////                            }
+//                    if(task.isSuccessful())
+//                    {
+//                        currentuser=mAuth.getCurrentUser();
+//                        String userEmail=currentuser.getEmail();
+//                        Toast.makeText(getApplicationContext(), "Account created successfully!  with email Id"+ userEmail, Toast.LENGTH_SHORT).show();
+//                        Intent intent=new Intent(getApplicationContext(),home.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                    else{
+//                        Toast.makeText(getApplicationContext(), "Error While Creating Account with email id "+task.getException(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+        Intent intent=new Intent(getApplicationContext(),create_account.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        email = et_email.getText().toString();
+        password = et_pass.getText().toString();
+        if (password.length() < 8) {
+            et_pass.setError("Enter correct password");
+            et_pass.requestFocus();
+            return;
+        } else if (TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            et_email.setError("Enter Valid Email Address!");
+            et_email.requestFocus();
+            return;
+        } else {
+
+            ActionProcessButton btn = (ActionProcessButton) view;
+            // we add 25 in the button progress each click
+            if(btn.getProgress() < 100){
+                btn.setProgress(btn.getProgress() + 15);
+                et_email.setEnabled(false);
+                et_pass.setEnabled(true);
             }
-        });
-        btnLogin.setOnClickListener(this);
-
-
-    }
-
-    private void userLogin(){
-        String email = etEmail.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //if the email and password are not empty
-        //displaying a progress dialog
-
-        progressDialog.setMessage("Checking credentials Please Wait");
-        progressDialog.show();
-
-        mAuth.signInWithEmailAndPassword(email,password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful())
-                        {
-                            finish();
-                            startActivity(new Intent(getApplicationContext(),home.class));
-                        }
+            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful())
+                    {
+                        currentuser=mAuth.getCurrentUser();
+                        String userEmail=currentuser.getEmail();
+                        Toast.makeText(getApplicationContext(), "Login successful with email id "+userEmail, Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(getApplicationContext(),home.class);
+                        startActivity(intent);
+                        finish();
                     }
-                });
+                    else{
+                        Toast.makeText(getApplicationContext(), "Please Enter Valid credentials "+task.getException(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+        }
     }
 
-    @Override
-    public void onClick(View view) {
-        if(view == btnLogin){
-            userLogin();
-        }
+    public void forgot(View view) {
+
+        Intent intent=new Intent(getApplicationContext(),create_account.class);
+        startActivity(intent);
+        //finish();
     }
 }
--- app/src/main/java/com/fcproject/grabhouce/create_account.java
+++ app/src/main/java/com/fcproject/grabhouce/create_account.java
@@ -10,11 +10,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.dd.processbutton.iml.ActionProcessButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInApi;
@@ -30,27 +32,33 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class create_account extends AppCompatActivity implements View.OnClickListener {
+public class create_account extends AppCompatActivity {
 
     Button btnSignup, btnCancel, btnFacebook;
 
     GoogleSignInClient mGoogleSignInClient;
-    EditText etEmail, etPassword;
+    EditText etEmail, etPassword,et_email,et_pass;
     private ProgressDialog progressDialog;
     int RC_SIGN_IN=234;
     private FirebaseAuth mAuth;
-
+    String email,password;
+    FirebaseUser currentuser;
+    ActionProcessButton btnFirebaseSignUp,btnFirebaseLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
-        btnSignup = findViewById(R.id.btnSignUp);
-        btnCancel = findViewById(R.id.btnCancel);
+        btnFirebaseSignUp = findViewById(R.id.btnFirebaseSignUp);
+        btnFirebaseSignUp.setMode(ActionProcessButton.Mode.ENDLESS);
+        //start with progress = 0
+        btnFirebaseSignUp.setProgress(0);
+        et_email=findViewById(R.id.ed_email);
+        et_pass=findViewById(R.id.ed_password);
         mAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
-        etEmail = findViewById(R.id.etEmail);
-        etPassword = findViewById(R.id.etPassword);
+//        etEmail = findViewById(R.id.etEmail);
+//        etPassword = findViewById(R.id.etPassword);
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
@@ -61,65 +69,111 @@ public class create_account extends AppCompatActivity implements View.OnClickLis
 
 
 
-        btnSignup.setOnClickListener(this);
+     //   btnSignup.setOnClickListener(this);
 
-        findViewById(R.id.btnGoogle).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
+//        findViewById(R.id.btnGoogle).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                signIn();
+//
+//            }
+//        });
 
-            }
-        });
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                etEmail.getText().clear();
-                etPassword.getText().clear();
-            }
-        });
-
-    }//on create close
-
-    public void onClick(View view){
-        if(view == btnSignup){
-            registerUser();
-        }
+//        btnCancel.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                etEmail.getText().clear();
+//                etPassword.getText().clear();
+//            }
+//        });
+//
+//    }//on create close
+//
+//    public void onClick(View view){
+//        if(view == btnSignup){
+//            registerUser();
+//        }
     }
 
-    private void registerUser() {
-        String email = etEmail.getText().toString().trim();
-        String password = etPassword.getText().toString().trim();
-        if(TextUtils.isEmpty(email)){
-            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        if(TextUtils.isEmpty(password)){
-            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
-            return;
-        }
-
-        //if the email and password are not empty
-        //displaying a progress dialog
-
-        progressDialog.setMessage("Registering You");
-        progressDialog.show();
-
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(create_account.this, "Success ! Start Exploring", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(getApplicationContext(),home.class));
-                }
-                else{
-                    Toast.makeText(create_account.this, "Registration Error", Toast.LENGTH_SHORT).show();
-                }
-                progressDialog.dismiss();
-            }
-        });
-    }
+//    private void registerser(View view) {
+////        String email = etEmail.getText().toString().trim();
+////        String password = etPassword.getText().toString().trim();
+////        if(TextUtils.isEmpty(email)){
+////            Toast.makeText(this,"Please enter email",Toast.LENGTH_LONG).show();
+////            return;
+////        }
+////
+////        if(TextUtils.isEmpty(password)){
+////            Toast.makeText(this,"Please enter password",Toast.LENGTH_LONG).show();
+////            return;
+////        }
+////
+////        //if the email and password are not empty
+////        //displaying a progress dialog
+////
+////        progressDialog.setMessage("Registering You");
+////        progressDialog.show();
+////
+////        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+////            @Override
+////            public void onComplete(@NonNull Task<AuthResult> task) {
+////                if(task.isSuccessful()){
+////                    Toast.makeText(create_account.this, "Success ! Start Exploring", Toast.LENGTH_LONG).show();
+////                    startActivity(new Intent(getApplicationContext(),home.class));
+////                }
+////                else{
+////                    Toast.makeText(create_account.this, "Registration Error", Toast.LENGTH_SHORT).show();
+////                }
+////                progressDialog.dismiss();
+////            }
+////        });
+//        email =et_email.getText().toString();
+//        password=et_pass.getText().toString();
+//
+//        if(password.length()<8)
+//        {
+//            et_pass.setError("Please enter password of minimum length 8");
+//            et_pass.requestFocus();
+//            return;
+//        }
+//        else if(TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+//        {
+//            et_email.setError("Enter Valid Email id");
+//            et_email.requestFocus();
+//            return;
+//        }
+//        else
+//        {
+//            ActionProcessButton btn = (ActionProcessButton) v;
+//            // we add 25 in the button progress each click
+//            if(btn.getProgress() < 100){
+//                btn.setProgress(btn.getProgress() + 15);
+//                et_email.setEnabled(false);
+//                et_pass.setEnabled(false);
+//            }
+//            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+////                            if(currentuser!=null)
+////                            {
+////                                Toast.makeText(LoginActivity.this, "Account created successfully!  with email Id"+email, Toast.LENGTH_SHORT).show();
+////                            }
+//                    if(task.isSuccessful())
+//                    {
+//                        currentuser=mAuth.getCurrentUser();
+//                        String userEmail=currentuser.getEmail();
+//                        Toast.makeText(getApplicationContext(), "Account created successfully!  with email Id"+ userEmail, Toast.LENGTH_SHORT).show();
+//                        Intent intent=new Intent(getApplicationContext(),home.class);
+//                        startActivity(intent);
+//                        finish();
+//                    }
+//                    else{
+//                        Toast.makeText(getApplicationContext(), "Error While Creating Account with email id "+task.getException(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
 
 
     public void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -164,10 +218,64 @@ public class create_account extends AppCompatActivity implements View.OnClickLis
                     }
                 });
     }
-    private void signIn()
-    {
+//    private void GooglesignIn()
+//    {
+//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
+
+    public void GooglesignIn(View view) {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void registerUser(View view) {
+        email =et_email.getText().toString();
+        password=et_pass.getText().toString();
+
+        if(password.length()<8)
+        {
+            et_pass.setError("Please enter password of minimum length 8");
+            et_pass.requestFocus();
+            return;
+        }
+        else if(TextUtils.isEmpty(email) || !Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            et_email.setError("Enter Valid Email id");
+            et_email.requestFocus();
+            return;
+        }
+        else
+        {
+            ActionProcessButton btn = (ActionProcessButton) view;
+            // we add 25 in the button progress each click
+            if(btn.getProgress() < 100){
+                btn.setProgress(btn.getProgress() + 15);
+                et_email.setEnabled(false);
+                et_pass.setEnabled(false);
+            }
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if(currentuser!=null)
+//                            {
+//                                Toast.makeText(LoginActivity.this, "Account created successfully!  with email Id"+email, Toast.LENGTH_SHORT).show();
+//                            }
+                    if(task.isSuccessful())
+                    {
+                        currentuser=mAuth.getCurrentUser();
+                        String userEmail=currentuser.getEmail();
+                        Toast.makeText(getApplicationContext(), "Account created successfully!  with email Id"+ userEmail, Toast.LENGTH_SHORT).show();
+                        Intent intent=new Intent(getApplicationContext(),home.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Error While Creating Account with email id "+task.getException(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 }
 
